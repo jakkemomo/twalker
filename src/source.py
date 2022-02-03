3e2854ad741ce54172202bce099423da007c9f1d
jaqombo <alexolikov@gmail.com>
2022-02-04 01:12:54 +0300

    Refactoring, change prints for lazy logging

--- src/source.py
+++ src/source.py
@@ -49,25 +49,26 @@ class TwitterSource(InformationSource):
 
     @staticmethod
     def _create_url(user_id, start_date, end_date, max_results=10):
-        search_url = "https://api.twitter.com/2/tweets/search/all"  # Change to the endpoint you want to collect data from
+        search_url = "https://api.twitter.com/2/tweets/search/all"
 
-        # change params based on the endpoint you are using
-        query_params = {'query': user_id,
-                        'start_time': start_date,
-                        'end_time': end_date,
-                        'max_results': max_results,
-                        'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
-                        'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
-                        'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
-                        'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
-                        'next_token': {}}
+        query_params = {
+            'query': user_id,
+            'start_time': start_date,
+            'end_time': end_date,
+            'max_results': max_results,
+            'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+            'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+            'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+            'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+            'next_token': {}
+        }
         return search_url, query_params
 
     @staticmethod
     def _connect_to_endpoint(url, headers, params, next_token=None) -> dict:
-        params['next_token'] = next_token  # params object received from create_url function
+        params['next_token'] = next_token
         response = requests.request("GET", url, headers=headers, params=params)
-        print("Endpoint Response Code: " + str(response.status_code))
+        logging.debug("Endpoint Response Code: %s", str(response.status_code))
         if response.status_code != 200:
             raise ConnectionError(response.status_code, response.text)
         return response.json()
